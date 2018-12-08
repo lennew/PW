b00abbd444278a888c43551d4d5fe454199042de
lennew <42210473+lennew@users.noreply.github.com>
2018-12-08 13:49:27 +0100

    Update semafory.c

--- semafory.c
+++ semafory.c
@@ -14,6 +14,7 @@
 #include <time.h>
 
 int semid;
+const int liczbaFilozofow = 5;
 static struct sembuf buf;
 
 void podnies(int semid, int semnum) {
@@ -39,42 +40,67 @@ void opusc (int semid, int semnum) {
 }
 
 void handler () {
-    if (semctl(semid, 0, IPC_RMID, 0) == -1) {
+    if (semctl(semid, 0, IPC_RMID, NULL) == -1) {
         perror("cannot remove semaphore");
         exit(1);
     }
     exit(0);
 }
 
-int tworzFilozofa (int numer) {
+void posadzFilozofa (int numer) {
     if (fork() == 0) {
-        write (1, numer);
-        perror ("Cannot create child")
-        exit(1);
+        int mniejszy = numer;
+        int wiekszy = numer + 1;
+        
+        char sMniejszy[6];
+        strcpy(sMniejszy, "lewy");
+        char sWiekszy[6];
+        strcpy(sWiekszy, "prawy");
+        
+        while(1) {
+            if (numer + 1 == liczbaFilozofow) {
+                mniejszy = 0;
+                wiekszy = numer;
+                strcpy(sMniejszy, "prawy");
+                strcpy(sWiekszy, "lewy");
+            }
+            opusc(semid, wiekszy);
+            printf("Filozof %d: Wzialem widelec %s\n", numer, sWiekszy);
+            opusc(semid, mniejszy);
+            printf("Filozof %d: Wzialem widelec %s\n", numer, sMniejszy);
+            sleep(rand() % 3);
+            printf("Filozof %d: Zjadlem\n", numer);
+            podnies(semid, mniejszy);
+            printf("Filozof %d: Polozylem widelec %s\n", numer, sMniejszy);
+            podnies(semid, wiekszy);
+            printf("Filozof %d: Polozylem widelec %s\n", numer, sWiekszy);
+        }
     }
-    opusc(semid, numer - 1);
-    printf("Filozof:%d: Wzialem widelec lewy\n", numer);
-    opusc(semid, numer);
-    printf("Filozof:%d: Wzialem widelec prawy\n", numer);
-    sleep(rand() % 4);
-    printf("Filozof:%d: Zjadlem\n");
-    podnies(semid, numer);
-    printf("Filozof:%d: Polozylem widelec prawy\n", numer);
-    opusc(semid, numer);
-    printf("Filozof:%d: Polozylem widelec lewy\n", numer);
 }
 
-
-int main() {  
-    if ((semid = semget(0x112, 1, IPC_CREAT | 0777)) == -1) {
+void nakryjDoStolu () {
+    if ((semid = semget(0x997, liczbaFilozofow, IPC_CREAT | 0777)) == -1) {
         perror("Cannot get semaphore");
         exit(1);
     }
-    if (semctl(semid, 0, SETVAL, 1) == -1) {
-        perror("Cannot set semaphore's value");
-        exit(1);
-    }
     
+    for (int i = 0; i < liczbaFilozofow; ++i) {
+        if (semctl(semid, i, SETVAL, 1) == -1) {
+            perror("Cannot set semaphore's value");
+            exit(1);
+        }
+    }
+}
+
+
+
+int main() {  
     srand(time(NULL));
-    exit(1);
+    
+    nakryjDoStolu();
+    for (int i = 0; i < liczbaFilozofow ; ++i) posadzFilozofa(i);
+    
+    signal(SIGINT, handler);
+    while(1);
+    exit(0);
 }
